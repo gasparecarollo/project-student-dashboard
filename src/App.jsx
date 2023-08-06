38fbc0aba79cef69cb6a86ee594f1a6aaa896ec3
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-08-06 12:05:07 -0400

    Show more/show Less does not work. Working on sorting Cohort

--- src/App.jsx
+++ src/App.jsx
@@ -2,58 +2,63 @@ import React from "react";
 import { useState } from "react";
 import data from "./data/data.json"
 import studentDetails from "./Components/studentDetails";
-import "./App.css"
+// import "App.css"
 
 
 console.log("data[0] : ", data[0])
 
 function App() {
-  const [showMore, setShowMore] = useState(false);
-
+  const [trackSelectedStudentByID, setTrackSelectedStudentByID] = useState(null);
+  const [showCertifs, setShowCertifs] = useState(false);
 
   const studentsToRender = data.map(
-    (eachStudentObj) => {
-      return (
-        <>
-          <div className="card_container">
+    (eachStudentObj) => (
 
-            <img src={eachStudentObj.profilePhoto} alt={`Image of Student: ${eachStudentObj.names.preferredName}`} />
-            <h4>
-              <div className="studentName" id={eachStudentObj.id}>
-                {`Student Name: ${eachStudentObj.names.preferredName}`}
-              </div>
-              <div className="email">
-                {`Email: ${eachStudentObj.username}`}
-              </div>
-              <div>{`Birthday: ${eachStudentObj.dob}`}
-                <div className="startDate">
-                  {`Date Started: ${eachStudentObj.cohort.cohortCode}`}
-                </div>
-                <div className="Show">
-                  <button type="submit" onClick={() => setShowMore(!showMore)}> ...show more</button>
-                  {showMore && <studentDetails />}
-                </div>
-              </div>
+      <div className="card_container" key={eachStudentObj.id}>
 
-            </h4>
+        <img src={eachStudentObj.profilePhoto} alt={`Image of Student: ${eachStudentObj.names.preferredName}`} />
 
-            <div>
+        <h4>
+          <div className="studentName" id={eachStudentObj.id}>
+            {`Student Name: ${eachStudentObj.names.preferredName}`}
+          </div>
+          <div className="email">
+            {`Email: ${eachStudentObj.username}`}
+          </div>
+          <div>{`Birthday: ${eachStudentObj.dob}`}
+            <div className="startDate">
+              {`Date Started: ${eachStudentObj.cohort.cohortCode}`}
+            </div>
+            <div className="Show">
+              <button
+                type="submit"
+                onClick={() =>
+                  setTrackSelectedStudentByID(trackSelectedStudentByID === eachStudentObj.id ? null : eachStudentObj.id
+                  )
+                }
+              >
+                {trackSelectedStudentByID === eachStudentObj.id ? "Show Less" : "Show More"}
+              </button>
+              {trackSelectedStudentByID === eachStudentObj.id && (
+                <studentDetails student={eachStudentObj} />
 
-              <p>
+              )}
 
-
-
-              </p>
+              {/* <button type="submit onClick={() => setShowCertifs(!showCertifs)}> 
+                {showCertifs ? "Show Less" : "Show More"}
+              </button>
+              {showCertifs && studentDetails}
+              {showCertifs && <studentDetails />} */}
 
             </div>
 
-          </div >
-        </>
-      )
+          </div>
 
+        </h4 >
 
+      </div >
 
-    }
+    )
   )
 
   const cohortsToRender = data.map(
@@ -70,21 +75,23 @@ function App() {
   )
 
 
-
-
-
-
-
   return (
     <div>
       <h1 className="header">Student Dashboard</h1>
       {studentsToRender}
-      <h3>Choose A Class By Start Date</h3>
-      {cohortsToRender}
+      <h2>Choose A Class By Start Date</h2>
+      {/* {cohortsToRender} */}
 
-      <div>
-        <h4> Student Details</h4>
-        <button></button>
+      <div className="studentDeets">
+        <h2> Student Details</h2>
+        {studentsToRender}
+        {showCertifs && studentsToRender.map(eachStudentObj => (
+          <studentDetails key={eachStudentObj.id} />
+
+
+
+        ))}
+
 
       </div>
     </div>
--- src/Components/studentDetails.jsx
+++ src/Components/studentDetails.jsx
@@ -1,22 +1,22 @@
 import data from "../data/data.json"
 
-const studentDetails = data.map(
-    (studentDetails) => {
-        return (
+const studentDetails = ({ studentdeets }) => {
 
-            <div className="details">
-                <p>{`Resume Certification Status: ${studentDetails.certifications.resume ? "👍🏻" : "👎🏻"}`} </p>
-                <p>{`LinkedIn Profile Status: ${studentDetails.linkedin ? "👍🏻" : "👎🏻"}`} </p>
-                <p>{`GitHub Profile Status: ${studentDetails.github ? "👍🏻" : "👎🏻"}`} </p>
-                <p>{`Mock Interview Status: ${studentDetails.mockInterview ? "👍🏻" : "👎🏻"}`} </p>
-                <p>{`CodeWars Score Percentage: ${(parseInt(studentDetails.codewars.current.total) / parseInt(studentDetails.codewars.goal.total) * 100).toFixed(2)} %`}</p>
-                <p>{`CodeWars Score above 600: ${parseInt(studentDetails.codewars.current.total) <= 600 ? "👍🏻" : "👎🏻"}`}</p>
-            </div>
+    return (
+
+        <div className="details">
+            <p>{`Resume Certification Status: ${studentdeets.certifications.resume ? "👍🏻" : "👎🏻"}`} </p>
+            <p>{`LinkedIn Profile Status: ${studentdeets.linkedin ? "👍🏻" : "👎🏻"}`} </p>
+            <p>{`GitHub Profile Status: ${studentdeets.github ? "👍🏻" : "👎🏻"}`} </p>
+            <p>{`Mock Interview Status: ${studentdeets.mockInterview ? "👍🏻" : "👎🏻"}`} </p>
+            <p>{`CodeWars Score Percentage: ${studentdeets.codewars && (parseInt(studentdeets.codewars.current.total) / parseInt(studentdeets.codewars.goal.total) * 100).toFixed(2)} %`}</p>
+            <p>{`CodeWars Score above 600: ${studentdeets.codewars && parseInt(studentdeets.codewars.current.total) <= 600 ? "👍🏻" : "👎🏻"}`}</p>
+        </div>
 
 
-        )
-    }
-)
+    );
+
+};
 
 
 export default studentDetails;
